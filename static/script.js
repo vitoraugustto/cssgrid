13d32bc9f0ded55869439fa951a9dc8f2226dfc0
vitoraugustto <60718553+vitoraugustto@users.noreply.github.com>
2020-12-09 12:25:57 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+
+    const hamburguer = document.getElementById('hamburguer');
+    const header = document.querySelector('header');
+    const nav = document.querySelector('nav');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+
+    var hamburguerState = 0
+    hamburguer.addEventListener('click', function(){
+        hamburguerState += 1;
+        
+        if(hamburguerState == 1){
+            nav.style.display = 'none';
+            header.style.gridColumn = '1 / span 3';
+            main.style.gridColumn = '1  / span 3';
+            footer.style.gridColumn = '1 / span 3';
+            
+        } else {
+            nav.style.display = 'inline-block';
+            header.style.gridColumn = '2 / span 3';
+            main.style.gridColumn = '2  / span 3';
+            footer.style.gridColumn = '1 / span 4';
+
+            hamburguerState = 0;
+        }
+
+    });
+
+};
