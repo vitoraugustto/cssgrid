b56abae89816c4972b630f5342751d599d507580
vitoraugustto <60718553+vitoraugustto@users.noreply.github.com>
2020-12-10 08:26:00 -0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,10 +1,14 @@
 window.onload = function(){
 
-    const hamburguer = document.getElementById('hamburguer');
     const header = document.querySelector('header');
     const nav = document.querySelector('nav');
     const main = document.querySelector('main');
     const footer = document.querySelector('footer');
+
+    const hamburguer = document.getElementById('hamburguer');
+    const boxes = document.getElementById('wrapper-boxes');
+    const lis = document.getElementsByTagName('li');
+
 
     var hamburguerState = 0
     hamburguer.addEventListener('click', function(){
@@ -25,5 +29,32 @@ window.onload = function(){
             hamburguerState = 0;
         };
     });
+
+    arrayLis = [...lis];
+
+    arrayLis.forEach((li) =>
+        li.addEventListener('mouseenter', function() {
+            li.classList.add('contrastIn');
+            li.classList.remove('contrastOut');
+        }));
+
+    arrayLis.forEach((li) => 
+        li.addEventListener('mouseout', function() {
+            li.classList.remove('contrastIn');
+            li.classList.add('contrastOut');
+        }));
+
+
+    function createBoxes(className) {
+        for ( var i = 0; i < 16; i++ ) {
+            const box = document.createElement('div');
+            //box.classList.add('box');
+
+            box.className = className;
+            boxes.appendChild(box);
+        };
+    }; 
+
+    createBoxes('box');
 
 };
